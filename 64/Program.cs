4a065f4744cd7e25e666989056c6598d7a041980
San <yan_san_din@mail.ru>
2023-03-02 10:08:35 +0300

    added new folder

---
+++ 64/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = InputInt("Введите число больше 0: ");
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Вы ввели отрицательное число");
+}
+Console.WriteLine(N(n, m));
+
+int N(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{N(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
