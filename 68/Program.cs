70be4ddf6a1112014d8c620e4fad71607ebae621
San <yan_san_din@mail.ru>
2023-03-02 10:17:14 +0300

    added new folder

---
+++ 68/Program.cs
@@ -0,0 +1,19 @@
+﻿int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+Console.WriteLine($"A({m}, {n}) = {AkkermanFunc(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int AkkermanFunc(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return AkkermanFunc(m - 1, 1);
+    else
+        return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+}
